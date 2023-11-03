0ce4ee970bddc91bc7902510b7c14304f15b27f8
Roque Castro Garzon <rokke1558@gmail.com>
2023-11-02 21:32:10 -0500

    ultimate

--- src/main/java/com/roquecastro/gatewayserver/config/SecurityConfig.java
+++ src/main/java/com/roquecastro/gatewayserver/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.roquecastro.gatewayserver.config;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableWebFluxSecurity
 public class SecurityConfig {
 	
 	
