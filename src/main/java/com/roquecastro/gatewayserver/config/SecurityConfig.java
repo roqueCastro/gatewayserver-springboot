3f4bbdd5a4ab5b58b82daeacead1721e35ff3b15
Roque Castro Garzon <rokke1558@gmail.com>
2023-11-02 21:11:19 -0500

    add properties

---
+++ src/main/java/com/roquecastro/gatewayserver/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.roquecastro.gatewayserver.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+	
+	
+
+}
